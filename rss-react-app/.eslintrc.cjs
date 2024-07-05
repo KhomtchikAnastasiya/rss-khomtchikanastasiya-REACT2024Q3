644e18a9d5de2b70eefd72b612d358ff48a652bf
Khomtchik Anastasiya <lawrik.nastik@gmail.com>
2024-07-05 15:48:25 +0300

    init: start project

---
+++ rss-react-app/.eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+    root: true,
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: [
+        'react-refresh',
+        '@typescript-eslint',
+        'react',
+        'react-compiler',
+        'prettier',
+    ],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        '@typescript-eslint/no-explicit-any': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react-compiler/react-compiler': 'error',
+    },
+}
